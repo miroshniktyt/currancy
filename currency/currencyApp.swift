bdb7ca67a3a45ea85e1792bc0aed651fed322abe
Anton Miroshnyk <42864033+miroshniktyt@users.noreply.github.com>
2024-12-01 16:58:25 +0100

    basic project scructure added

---
+++ currency/currencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  currencyApp.swift
+//  currency
+//
+//  Created by pc on 01.12.24.
+//
+
+import SwiftUI
+
+@main
+struct currencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hi :)")
+        }
+    }
+}
